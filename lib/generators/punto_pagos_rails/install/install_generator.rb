28d2818dae76ee584100ea7c8b30d271409edbd4
Leandro Segovia <ldlsegovia@gmail.com>
2014-09-27 22:16:21 -0300

    feat(generators): add install generator scaffold

---
+++ lib/generators/punto_pagos_rails/install/install_generator.rb
@@ -0,0 +1,15 @@
+class PuntoPagosRails::InstallGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+
+  def create_resource_model
+    puts 'TODO: create_resource_model'
+  end
+
+  def copy_migrations
+    puts 'TODO: copy_migrations'
+  end
+
+  def create_initializer
+    puts 'TODO: create_initializer'
+  end
+end
