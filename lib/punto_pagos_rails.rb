e322f7bd3a89bdd07e4cf7c6999b6c65bb856a87
juliogarciag <julioggonz@gmail.com>
2014-11-21 17:35:42 -0500

    feat(): Allowing to use rbenv-vars in puntopagos configuration

--- lib/punto_pagos_rails.rb
+++ lib/punto_pagos_rails.rb
@@ -1,16 +1,39 @@
-require "puntopagos"
 require "punto_pagos_rails/engine"
 require "punto_pagos_rails/resource_extension"
 require "punto_pagos_rails/transaction_service"
 
 module PuntoPagosRails
-  mattr_accessor :resource_class_name
+  extend self
 
-  def self.resource_class
+  attr_accessor :resource_class_name, :punto_pagos
+
+  def resource_class
     resource_class_name.constantize
   end
 
-  def self.setup
+  def punto_pagos
+    @punto_pagos ||= {}
+  end
+
+  def setup
     yield self
+    require "puntopagos"
+    load_puntopagos_configuration
+  end
+
+  def load_puntopagos_configuration
+    PuntoPagos::Config.class_eval do
+      def puntopagos_key
+        @puntopagos_key ||= PuntoPagosRails.punto_pagos[:key]
+      end
+
+      def puntopagos_secret
+        @puntopagos_secret ||= PuntoPagosRails.punto_pagos[:secret]
+      end
+
+      def puntopagos_base_url
+        @puntopagos_base_url ||= PuntoPagos::Config::PUNTOPAGOS_BASE_URL[PuntoPagosRails.punto_pagos[:env]]
+      end
+    end
   end
 end
--- spec/dummy/config/initializers/punto_pagos_rails.rb
+++ spec/dummy/config/initializers/punto_pagos_rails.rb
@@ -1,3 +1,8 @@
 PuntoPagosRails.setup do |config|
   config.resource_class_name = 'Ticket'
+  config.punto_pagos = {
+    key: ENV["PUNTO_PAGOS_KEY"],
+    secret: ENV["PUNTO_PAGOS_SECRET"],
+    env: ENV["PUNTO_PAGOS_ENV"]
+  }
 end
