754fda19acbdf8d4da90fa79b18cbc4189cc42bb
Leandro Segovia <ldlsegovia@gmail.com>
2014-12-14 14:16:50 -0300

    fix(): puntopagos credentials ignored from engine initializer

--- lib/generators/punto_pagos_rails/install/install_generator.rb
+++ lib/generators/punto_pagos_rails/install/install_generator.rb
@@ -19,7 +19,15 @@ class PuntoPagosRails::InstallGenerator < Rails::Generators::NamedBase
   end
 
   def create_initializer
+    template "puntopagos.yml", "config/puntopagos.yml"
     template "punto_pagos_rails.rb", "config/initializers/punto_pagos_rails.rb"
+  end
+
+  def mount_routes
+    line = "Rails.application.routes.draw do"
+    gsub_file "config/routes.rb", /(#{Regexp.escape(line)})/mi do |match|
+      "#{match}\n mount PuntoPagosRails::Engine => '/'\n"
+    end
   end
 
   def copy_views
--- lib/punto_pagos_rails.rb
+++ lib/punto_pagos_rails.rb
@@ -5,35 +5,14 @@ require "punto_pagos_rails/transaction_service"
 module PuntoPagosRails
   extend self
 
-  attr_accessor :resource_class_name, :punto_pagos
+  attr_accessor :resource_class_name
 
   def resource_class
     resource_class_name.constantize
   end
 
-  def punto_pagos
-    @punto_pagos ||= {}
-  end
-
   def setup
     yield self
     require "puntopagos"
-    load_puntopagos_configuration
-  end
-
-  def load_puntopagos_configuration
-    PuntoPagos::Config.class_eval do
-      def puntopagos_key
-        @puntopagos_key ||= PuntoPagosRails.punto_pagos[:key]
-      end
-
-      def puntopagos_secret
-        @puntopagos_secret ||= PuntoPagosRails.punto_pagos[:secret]
-      end
-
-      def puntopagos_base_url
-        @puntopagos_base_url ||= PuntoPagos::Config::PUNTOPAGOS_BASE_URL[PuntoPagosRails.punto_pagos[:env]]
-      end
-    end
   end
 end
