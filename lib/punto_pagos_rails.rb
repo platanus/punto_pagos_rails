51a6a27017cddf4a4150216c3d7c433262873b6f
Leandro Segovia <ldlsegovia@gmail.com>
2016-06-06 15:13:57 -0300

    fix(): require puntopags outside of no more used setup method

--- lib/punto_pagos_rails.rb
+++ lib/punto_pagos_rails.rb
@@ -1,12 +1,7 @@
+require "puntopagos"
 require "punto_pagos_rails/engine"
 require "punto_pagos_rails/payable"
 require "punto_pagos_rails/transaction_service"
 
 module PuntoPagosRails
-  extend self
-
-  def setup
-    yield self
-    require "puntopagos"
-  end
 end
